59bb6221d67a31670382401b4a13212d30a6777c
matteospanio <spanio@dei.unipd.it>
2026-02-25 11:55:06 +0100

    🐛 Fix runtime import error
    main functions called before import

--- lilybert/cli/__init__.py
+++ lilybert/cli/__init__.py
@@ -1,21 +1,5 @@
 """CLI subcommand modules for lilyBERT workflows."""
 
-from . import (
-    combine,
-    evaluate,
-    generate_tables,
-    mutopia_preprocess,
-    predict,
-    preprocess,
-    pretokenize,
-    pretrain,
-    run_experiment,
-    train,
-    train_tokenizer,
-    upload_dataset,
-    upload_model,
-)
-
 __all__ = [
     "evaluate",
     "generate_tables",
